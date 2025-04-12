3dd5b8aba70012e50898b37443957713de8f5dbb
Alex T <aletapia@proton.me>
2025-04-11 22:34:36 -0600

    (chore): update font settings in Ghostty and Cursor configurations, enhance Neovim setup with new plugins and keybindings for improved functionality.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,7 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -98,6 +102,11 @@ require("lazy").setup({
       statusline.section_location = function()
         return "%2l:%-2v"
       end
+      require("mini.pairs").setup()
+      require("mini.completion").setup()
+      require("mini.snippets").setup()
+      require("mini.icons").setup()
+      MiniIcons.tweak_lsp_kind()
     end,
   },
   {
@@ -105,11 +114,66 @@ require("lazy").setup({
     "neovim/nvim-lspconfig",
     config = function()
       local lsp = require("lspconfig")
+      -- Rust
       lsp.rust_analyzer.setup({
         settings = {
           ["rust-analyzer"] = {},
         },
       })
+      -- Typescript
+      lsp.ts_ls.setup({})
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    event = "VimEnter",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable("make") == 1
+        end,
+      },
+      { "nvim-telescope/telescope-ui-select.nvim" },
+      { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+    },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown(),
+          },
+        },
+      })
+
+      pcall(require("telescope").load_extension, "fzf")
+      pcall(require("telescope").load_extension, "ui-select")
+
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>sh", builtin.help_tags)
+      vim.keymap.set("n", "<leader>sk", builtin.keymaps)
+      vim.keymap.set("n", "<leader>sf", builtin.find_files)
+      vim.keymap.set("n", "<leader>ss", builtin.builtin)
+      vim.keymap.set("n", "<leader>sw", builtin.grep_string)
+      vim.keymap.set("n", "<leader>sg", builtin.live_grep)
+      vim.keymap.set("n", "<leader>sd", builtin.diagnostics)
+      vim.keymap.set("n", "<leader>sr", builtin.resume)
+      vim.keymap.set("n", "<leader>s.", builtin.oldfiles)
+      vim.keymap.set("n", "<leader><leader>", builtin.buffers)
+
+      vim.keymap.set("n", "<leader>s/", function()
+        builtin.live_grep({
+          grep_open_files = true,
+          prompt_title = "Live Grep in Open Files",
+        })
+      end, { desc = "[S]earch [/] in Open Files" })
+
+      vim.keymap.set("n", "<leader>sn", function()
+        builtin.find_files({ cwd = vim.fn.stdpath("config") })
+      end, { desc = "[S]earch [N]eovim files" })
     end,
   },
 })
