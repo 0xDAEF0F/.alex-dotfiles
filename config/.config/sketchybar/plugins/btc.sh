8ea8c5b9edf226325c3a43bda94feb681977de4f
Alex T <aletapia@proton.me>
2025-06-02 20:10:03 -0500

    chore: update Rust formatting settings to disable single-line function formatting, enhance Cursor user settings with new HTML formatter, and adjust Bitcoin change percentage calculation in btc.sh script

--- config/.config/sketchybar/plugins/btc.sh
+++ config/.config/sketchybar/plugins/btc.sh
@@ -4,7 +4,7 @@
 BTC_INFO=$($HOME/.local/bin/prices info btc)
 
 BTC_PRICE=$(echo "$BTC_INFO" | jq -r '.[0].price | tonumber | floor')
-BTC_CHANGE=$(echo "$BTC_INFO" | jq -r '.[0].one_day_change_pct | tonumber | . * 100 | round | . * 0.01')
+BTC_CHANGE=$(echo "$BTC_INFO" | jq -r '.[0].one_day_change_pct')
 
 LABEL="${BTC_PRICE} ${BTC_CHANGE}%"
 
