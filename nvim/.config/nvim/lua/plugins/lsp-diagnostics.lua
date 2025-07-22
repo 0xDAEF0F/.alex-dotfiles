cc545dc1f833b1f43afc17444a5964b76ae602c3
Alex T <aletapia@proton.me>
2025-07-21 22:19:05 -0600

    diagnostics new

--- nvim/.config/nvim/lua/plugins/lsp-diagnostics.lua
+++ nvim/.config/nvim/lua/plugins/lsp-diagnostics.lua
@@ -3,6 +3,14 @@ return {
   event = "VeryLazy", -- Or `LspAttach`
   priority = 1000, -- needs to be loaded in first
   config = function()
-    require("tiny-inline-diagnostic").setup()
+    require("tiny-inline-diagnostic").setup({
+      preset = "powerline",
+      options = {
+        multilines = {
+          enabled = true,
+          always_show = false,
+        },
+      },
+    })
   end,
 }
