b6b215716bb40b6c53434c5f72281b8870dd604a
Alex T <aletapia@proton.me>
2025-08-02 01:46:16 -0600

    aaaaaaaaaa

--- nvim/.config/nvim/lua/plugins/fff.lua
+++ nvim/.config/nvim/lua/plugins/fff.lua
@@ -1,7 +1,13 @@
 return {
-  "dmtrKovalenko/fff.nvim",
-  build = "cargo build --release",
-  opts = {},
+  -- "dmtrKovalenko/fff.nvim",
+  -- build = "cargo build --release",
+  dir = "/Users/ale/mimi/erepos/fff.nvim",
+  name = "fff.nvim",
+  opts = {
+    debug = {
+      show_scores = true,
+    },
+  },
   keys = {
     {
       "<C-p>",
--- nvim/.config/nvim/lua/plugins/neoscroll.lua
+++ nvim/.config/nvim/lua/plugins/neoscroll.lua
@@ -1,15 +1,9 @@
 return {
   "karb94/neoscroll.nvim",
   config = function()
-    require("neoscroll").setup({
-      mappings = {
-        "<C-u>",
-        "<C-d>",
-        "<C-b>",
-        "zt",
-        "zz",
-        "zb",
-      },
+    local neoscroll = require("neoscroll")
+    neoscroll.setup({
+      mappings = {},
       hide_cursor = true,
       stop_eof = true,
       respect_scrolloff = false,
@@ -19,5 +13,28 @@ return {
       post_hook = nil,
       performance_mode = false,
     })
+
+    local keymap = {
+      ["<C-u>"] = function()
+        neoscroll.ctrl_u({ duration = 100 })
+      end,
+      ["<C-d>"] = function()
+        neoscroll.ctrl_d({ duration = 100 })
+      end,
+      ["zt"] = function()
+        neoscroll.zt({ half_win_duration = 100 })
+      end,
+      ["zz"] = function()
+        neoscroll.zz({ half_win_duration = 100 })
+      end,
+      ["zb"] = function()
+        neoscroll.zb({ half_win_duration = 100 })
+      end,
+    }
+    local modes = { "n", "v", "x" }
+    for key, func in pairs(keymap) do
+      vim.keymap.set(modes, key, func)
+    end
   end,
+  enabled = true,
 }
